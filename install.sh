ad6a2d6c004832e695ea1a04e7a13aa6465b3112
liyi16 <liyi16@meituan.com>
2015-09-29 13:18:51 +0800

    snippets to repo, and auto install files

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 cwd=`pwd`
+
 ln -s ${cwd}/.vimrc ~/.vimrc
 ln -s ${cwd}/.zshrc ~/.zshrc
+
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install plugins
+vim +PluginInstall +qa
+
+# add snippets
+snippetsDir="${HOME}/.vim/bundle/snipMate/snippets"
+rm $snippetsDir/_.snippets
+rm $snippetsDir/jsx.snippets
+ln -s ${cwd}/snippets/_.snippets $snippetsDir/_.snippets
+ln -s ${cwd}/snippets/jsx.snippets $snippetsDir/jsx.snippets
+
 
 sudo npm install instant-markdown-d -g --verbose
 
